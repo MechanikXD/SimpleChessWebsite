ef8001a8a8bd9fdb94be337150a4ffbb7ac99684
Andrii <101969951+MechanikXD@users.noreply.github.com>
2024-12-15 18:55:36 +0200

    Add simple chess board to website

---
+++ ChessSite/www/scripts.js
@@ -0,0 +1,10 @@
+const config = {
+    draggable: true,
+    dropOffBoard: 'snapback',
+    position: 'start',
+    moveSpeed: 'slow',
+    snapbackSpeed: 500,
+    snapSpeed: 100,
+}
+
+const board1 = ChessBoard('myBoard', config);
